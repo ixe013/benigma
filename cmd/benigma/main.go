acedb699efdaa2a852fd205cafb3a839136aa8ea
ixe013 <guillaume@paralint.com>
2019-11-14 14:57:50 -0500

    Initial commit required to build

---
+++ cmd/benigma/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	benigma "github.com/vaups/benigma"
+	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/sdk/plugin"
+)
+
+func main() {
+	apiClientMeta := &api.PluginAPIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	flags.Parse(os.Args[1:])
+
+	tlsConfig := apiClientMeta.GetTLSConfig()
+	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
+
+	err := plugin.Serve(&plugin.ServeOpts{
+		BackendFactoryFunc: enigma_backend.Factory,
+		TLSProviderFunc:    tlsProviderFunc,
+	})
+	if err != nil {
+		logger := hclog.New(&hclog.LoggerOptions{})
+
+		logger.Error("Enigma plugin shutting down", "error", err)
+		os.Exit(1)
+	}
+}
