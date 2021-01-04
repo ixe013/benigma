391387a313ce6dec71e96e2458678b4e23178d64
ixe013 <guillaume@paralint.com>
2021-01-04 16:45:14 -0500

    Harmless :retab in Vim

--- builtin_models.go
+++ builtin_models.go
@@ -1,47 +1,47 @@
 package benigma
 
 import (
-    "github.com/emedvedev/enigma"
+	"github.com/emedvedev/enigma"
 )
 
 var builtinModels = map[string]enigma.Enigma{
-    "I": enigma.Enigma{
-        Reflector: *enigma.NewReflector("YRUHQSLDPXNGOKMIEBFZCWVJAT", "B"),
-        Rotors: []*enigma.Rotor{
-            enigma.NewRotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", "III", "V"),
-            enigma.NewRotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "II", "E"),
-            enigma.NewRotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "I", "Q"),
-        },
-    },
-    "M3": enigma.Enigma{
-        Reflector: *enigma.NewReflector("YRUHQSLDPXNGOKMIEBFZCWVJAT", "B"),
-        Rotors: []*enigma.Rotor{
-            enigma.NewRotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", "III", "V"),
-            enigma.NewRotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "II", "E"),
-            enigma.NewRotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "I", "Q"),
-        },
-    },
-    "M4": enigma.Enigma{
-        Reflector: *enigma.NewReflector("ENKQAUYWJICOPBLMDXZVFTHRGS", "B-thin"),
-        Rotors: []*enigma.Rotor{
-            enigma.NewRotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", "III", "V"),
-            enigma.NewRotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "II", "E"),
-            enigma.NewRotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "I", "Q"),
-        },
-    },
-    "IXE013": enigma.Enigma{
-        Reflector: *enigma.NewReflector("YIZMPKRSHAVCWTBODUQNGJLXFE", "A"),
-        Rotors: []*enigma.Rotor{
-            enigma.NewRotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "II", "E"),
-            enigma.NewRotor("LEYJVCNIXWPBQMDRTAKZGFUHOS", "Beta", ""),
-            enigma.NewRotor("NZJHGRCXMYSWBOUFAIVLPEKQDT", "VII", "ZM"),
-        },
-    },
+	"I": enigma.Enigma{
+		Reflector: *enigma.NewReflector("YRUHQSLDPXNGOKMIEBFZCWVJAT", "B"),
+		Rotors: []*enigma.Rotor{
+			enigma.NewRotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", "III", "V"),
+			enigma.NewRotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "II", "E"),
+			enigma.NewRotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "I", "Q"),
+		},
+	},
+	"M3": enigma.Enigma{
+		Reflector: *enigma.NewReflector("YRUHQSLDPXNGOKMIEBFZCWVJAT", "B"),
+		Rotors: []*enigma.Rotor{
+			enigma.NewRotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", "III", "V"),
+			enigma.NewRotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "II", "E"),
+			enigma.NewRotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "I", "Q"),
+		},
+	},
+	"M4": enigma.Enigma{
+		Reflector: *enigma.NewReflector("ENKQAUYWJICOPBLMDXZVFTHRGS", "B-thin"),
+		Rotors: []*enigma.Rotor{
+			enigma.NewRotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", "III", "V"),
+			enigma.NewRotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "II", "E"),
+			enigma.NewRotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "I", "Q"),
+		},
+	},
+	"IXE013": enigma.Enigma{
+		Reflector: *enigma.NewReflector("YIZMPKRSHAVCWTBODUQNGJLXFE", "A"),
+		Rotors: []*enigma.Rotor{
+			enigma.NewRotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "II", "E"),
+			enigma.NewRotor("LEYJVCNIXWPBQMDRTAKZGFUHOS", "Beta", ""),
+			enigma.NewRotor("NZJHGRCXMYSWBOUFAIVLPEKQDT", "VII", "ZM"),
+		},
+	},
 }
 
 func builtinModelNames() (names []string) {
-    for k := range builtinModels {
-        names = append(names, k)
-    }
-    return names
+	for k := range builtinModels {
+		names = append(names, k)
+	}
+	return names
 }
--- path_models.go
+++ path_models.go
@@ -1,113 +1,113 @@
 package benigma
 
 import (
-    "context"
+	"context"
 
-    "github.com/hashicorp/vault/sdk/framework"
-    "github.com/hashicorp/vault/sdk/logical"
+	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/logical"
 )
 
-func (b *backend) getPathForModel() *framework.Path {
-    return &framework.Path{
-        Pattern: "models/?$",
+func (b *enigmaBackend) getPathForModel() *framework.Path {
+	return &framework.Path{
+		Pattern: "models/?$",
 
-        Fields: map[string]*framework.FieldSchema{
-            "name": {
-                Type:        framework.TypeString,
-                Description: "The name of the Enigma model you are creating. Cannot be one of the build-in model names.",
-            },
-        },
+		Fields: map[string]*framework.FieldSchema{
+			"name": {
+				Type:        framework.TypeString,
+				Description: "The name of the Enigma model you are creating. Cannot be one of the build-in model names.",
+			},
+		},
 
-        Operations: map[logical.Operation]framework.OperationHandler{
-            logical.ListOperation: &framework.PathOperation{
-                Callback: b.listModels,
-                Summary:  "List the available Enigma machine models",
-            },
-            logical.CreateOperation: &framework.PathOperation{
-                Callback: b.createModel,
-                Summary:  "Creates an new custom Enigma machine model",
-            },
-            logical.UpdateOperation: &framework.PathOperation{
-                Callback: b.createModel,
-                Summary:  "Creates an new custom Enigma machine model",
-            },
-        },
+		Operations: map[logical.Operation]framework.OperationHandler{
+			logical.ListOperation: &framework.PathOperation{
+				Callback: b.listModels,
+				Summary:  "List the available Enigma machine models",
+			},
+			logical.CreateOperation: &framework.PathOperation{
+				Callback: b.createModel,
+				Summary:  "Creates an new custom Enigma machine model",
+			},
+			logical.UpdateOperation: &framework.PathOperation{
+				Callback: b.createModel,
+				Summary:  "Creates an new custom Enigma machine model",
+			},
+		},
 
-        HelpSynopsis:    pathModelHelpSyn,
-        HelpDescription: pathModelHelpDesc,
-    }
+		HelpSynopsis:    pathModelHelpSyn,
+		HelpDescription: pathModelHelpDesc,
+	}
 }
 
-func (b *backend) getPathForSpecificModelOperations() *framework.Path {
-    return &framework.Path{
-        Pattern: "models/" + framework.GenericNameRegex("name"),
-        Fields: map[string]*framework.FieldSchema{
-            "name": {
-                Type:        framework.TypeString,
-                Description: "The name of the Enigma model you are creating, or deleting.",
-            },
-        },
+func (b *enigmaBackend) getPathForSpecificModelOperations() *framework.Path {
+	return &framework.Path{
+		Pattern: "models/" + framework.GenericNameRegex("name"),
+		Fields: map[string]*framework.FieldSchema{
+			"name": {
+				Type:        framework.TypeString,
+				Description: "The name of the Enigma model you are creating, or deleting.",
+			},
+		},
 
-        Operations: map[logical.Operation]framework.OperationHandler{
-            logical.DeleteOperation: &framework.PathOperation{
-                Callback: b.deleteModel,
-                Summary:  "Deletes a custom Enigma machine model (builtin models can't be deleted).",
-            },
-        },
+		Operations: map[logical.Operation]framework.OperationHandler{
+			logical.DeleteOperation: &framework.PathOperation{
+				Callback: b.deleteModel,
+				Summary:  "Deletes a custom Enigma machine model (builtin models can't be deleted).",
+			},
+		},
 
-        HelpSynopsis:    pathModelHelpSyn,
-        HelpDescription: pathModelHelpDesc,
-    }
+		HelpSynopsis:    pathModelHelpSyn,
+		HelpDescription: pathModelHelpDesc,
+	}
 }
 
-func (b *backend) listModels(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-    entries, err := req.Storage.List(ctx, "models/")
-    if err != nil {
-        return nil, err
-    }
+func (b *enigmaBackend) listModels(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	entries, err := req.Storage.List(ctx, "models/")
+	if err != nil {
+		return nil, err
+	}
 
-    return logical.ListResponse(append(builtinModelNames(), entries...)), nil
+	return logical.ListResponse(append(builtinModelNames(), entries...)), nil
 }
 
-func (b *backend) createModel(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-    name := data.Get("name").(string)
+func (b *enigmaBackend) createModel(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	name := data.Get("name").(string)
 
-    model := &modelEntry{
-        Name: name,
-    }
+	model := &modelEntry{
+		Name: name,
+	}
 
-    // Convert the model to JSON to store it
-    jsonEntry, err := logical.StorageEntryJSON("models/"+name, model)
-    if err != nil {
-        return nil, err
-    }
-    if err := req.Storage.Put(ctx, jsonEntry); err != nil {
-        return nil, err
-    }
+	// Convert the model to JSON to store it
+	jsonEntry, err := logical.StorageEntryJSON("models/"+name, model)
+	if err != nil {
+		return nil, err
+	}
+	if err := req.Storage.Put(ctx, jsonEntry); err != nil {
+		return nil, err
+	}
 
-    return nil, nil
+	return nil, nil
 }
 
-func (b *backend) deleteModel(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+func (b *enigmaBackend) deleteModel(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 
-    name := data.Get("name").(string)
+	name := data.Get("name").(string)
 
-    //Check to see if the instance name is builtin (and therefore can't be deleted)
-    _, protected := builtinModels[name]
+	//Check to see if the instance name is builtin (and therefore can't be deleted)
+	_, protected := builtinModels[name]
 
-    if protected {
-        return logical.ErrorResponse("Cannot delete builtin model "+name), logical.ErrInvalidRequest
-    }
+	if protected {
+		return logical.ErrorResponse("Cannot delete builtin model " + name), logical.ErrInvalidRequest
+	}
 
-    if err := req.Storage.Delete(ctx, "models/"+name); err != nil {
-        return nil, err
-    }
+	if err := req.Storage.Delete(ctx, "models/"+name); err != nil {
+		return nil, err
+	}
 
-    return nil, nil
+	return nil, nil
 }
 
 type modelEntry struct {
-    Name string `json:name`
+	Name string `json:name`
 }
 
 const pathModelHelpSyn = `Operations on a given Enigma machine model`
